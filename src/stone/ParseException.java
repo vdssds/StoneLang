363368af9d4f6128a09d7fcc34c07630316162a9
zhengbaolin <zhengbaolin@163.com>
2016-08-09 13:00:33 +0800

    完成了词法分析以及语法分析树类的创建

---
+++ src/stone/ParseException.java
@@ -0,0 +1,31 @@
+package stone;
+
+import java.io.IOException;
+
+public class ParseException extends Exception {
+
+	private static final long serialVersionUID = 4681515126503949169L;
+
+	public ParseException(Token t) {
+		this("", t);
+	}
+
+	public ParseException(String msg, Token t) {
+		super("syntax error around " + location(t) + ". " + msg);
+	}
+
+	private static String location(Token t) {
+		if (t == Token.EOF)
+			return "the last line";
+		else
+			return "\"" + t.getText() + "\" at line " + t.getLineNumber();
+	}
+
+	public ParseException(IOException e) {
+		super(e);
+	}
+
+	public ParseException(String msg) {
+		super(msg);
+	}
+}
--- src/stone/ast/ASTree.java
+++ src/stone/ast/ASTree.java
@@ -1,6 +1,17 @@
 package stone.ast;
 
-public abstract class ASTree {
+import java.util.Iterator;
+
+public abstract class ASTree implements Iterable<ASTree> {
+	public abstract ASTree child(int i);
+
+	public abstract int numChildren();
+
+	public abstract Iterator<ASTree> children();
 
 	public abstract String location();
+
+	public Iterator<ASTree> iterator() {
+		return children();
+	}
 }
